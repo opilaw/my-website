7398f748a9a16982fa855dcc6a9f4b54b8a892d6
Olga.Pilawka <Olga.Pilawka-cic@ibm.com>
2023-12-19 14:12:04 +0100

    animation landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image"
 import portrait from '@/public/portrait.png'
-
+import { motion } from "framer-motion"
+import Portrait from "./components/portrait"
+import vector from '@/public/vector.svg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen  items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col md:flex-row 2xl:flex-row items-center justify-between p-24">
      <>
-      <div className="container mx-auto px-4">
+      <div className=" mx-auto px-4">
         <h1>Hello world</h1>
         <p>I am Olga Pilawka</p>
         <p>I am full time frontend developer and part time painter </p>
@@ -18,10 +20,16 @@ export default function Home() {
           ENJOY! 
         </h1>
       </div>
-      <div>
-        <Image src={portrait} alt="olga.pilawka" width={1000}
-      height={1000}></Image>
-      </div>
+     
+        {/* <Image src={portrait} alt="olga.pilawka" width={1000}
+      height={1000}></Image> */}
+    
+   <Portrait></Portrait>
+  <div className="mx-auto px-4">
+    <h1>I am graduated from Aalborg University in Copenhagen</h1>
+    <p>I hold MSc in IT in Information Studies major, with Web Development Specialization</p>
+    <p>In my free time besides learning new things, I paint. You can see my best works on this website</p>
+  </div>
     </>
     </main>
   )
